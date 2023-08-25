0b928173937abec42b2732ab1e2dcc116bafd71c
aleushea <aleushea@gmail.com>
2023-08-24 18:25:35 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- push.c
+++ push.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * f_push - function takes two parameters: head and counter for pushing a new node onto a stack 
+ * f_push - a fn takes two parameters: head and counter for pushing a new node
  * @head: stack head
  * @counter: line_number
  * Return: no return
@@ -8,10 +8,10 @@
 void f_push(stack_t **head, unsigned int counter)
 {
 	int n, each_digits = 0, flag = 0;
-	// check if an argument (bus.arg) that represents the value to be pushed onto the stack is provided
+	/*** check if an argument represents the value to be pushed to stack ***/
 	if (bus.arg)
 	{
-		// check if the number is negative
+		/*** check if the number is negative ***/
 		if (bus.arg[0] == '-')
 			each_digits++;
 		for (; bus.arg[each_digits] != '\0'; each_digits++)
